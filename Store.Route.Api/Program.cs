183a11ec03fcee9ce6c86bab82536706b6f8f98c
abdelazizamr33 <zizoamr920@gmail.com>
2025-04-20 22:52:15 +0200

    Session4

--- Store.Route.Api/Program.cs
+++ Store.Route.Api/Program.cs
@@ -1,10 +1,15 @@
 
 using Domain.Contracts;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 using Persistence.Data;
 using Services;
 using ServicesAbstractions;
+using Shared.errorModels;
+using Store.Route.Api.Extensions;
+using Store.Route.Api.Middlewares;
 using AssemblyRefMapping=Services.AssemblyReference;
 
 namespace Store.Route.Api
@@ -15,43 +20,14 @@ namespace Store.Route.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Services.RegisterAllServices(builder.Configuration);
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.AddDbContext<StoreDbContext>(options=>
-            {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-            });
-            builder.Services.AddScoped<IDbInitializer, DbIntializer>(); // Allow DI 
-            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-            builder.Services.AddScoped<IServiceManager, ServiceManager>();
-            builder.Services.AddAutoMapper(typeof(AssemblyRefMapping).Assembly);
             var app = builder.Build();
 
-            #region Seeding
-            using var scope = app.Services.CreateScope();
-            var dbIntializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();// Allow CR Create Object from DbIntializer
-            await dbIntializer.IntializeAsync(); 
-            #endregion
+            // after build configure any middleware
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-            app.UseStaticFiles();
-
-            app.MapControllers();
-
+            await app.ConfigureMiddleWares();
+            
             app.Run();
         }
     }
